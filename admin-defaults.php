12bb44064c095b7f331c4d8ec9a7942499afdc55
frankchester <fran@haselden.co.uk>
2014-10-16 20:49:13 +0100

    Fix admin panel widths

--- admin-defaults.php
+++ admin-defaults.php
@@ -2,32 +2,32 @@
 	<h3 class="admintitle">Recent Posts</h3>
 	<table border="0">
 		<tr>
-			<td><p>My First Post</p></td>
+			<td><p>An Example Post</p></td>
 			<td><span><a href=""><i class="fa fa-pencil"></i>Edit</a></span></td>
 			<td><span><a href=""><i class="fa fa-eye"></i>View</a></span></td>
 		</tr>
 		<tr>
-			<td><p>My Second Post</p></td>
+			<td><p>CSS-Only Solution For UI Tracking</p></td>
 			<td><span><a href=""><i class="fa fa-pencil"></i>Edit</a></span></td>
 			<td><span><a href=""><i class="fa fa-eye"></i>View</a></span></td>
 		</tr>
 		<tr>
-			<td><p>My Third Post</p></td>
+			<td><p>Ready For Retina HD</p></td>
 			<td><span><a href=""><i class="fa fa-pencil"></i>Edit</a></span></td>
 			<td><span><a href=""><i class="fa fa-eye"></i>View</a></span></td>
 		</tr>
 		<tr>
-			<td><p>My Fourth Post</p></td>
+			<td><p>Taking A Closer Look At Tech Conferences</p></td>
 			<td><span><a href=""><i class="fa fa-pencil"></i>Edit</a></span></td>
 			<td><span><a href=""><i class="fa fa-eye"></i>View</a></span></td>
 		</tr>
 		<tr>
-			<td><p>My Fifth Post</p></td>
+			<td><p>Wayfinding For The Mobile Web</p></td>
 			<td><span><a href=""><i class="fa fa-pencil"></i>Edit</a></span></td>
 			<td><span><a href=""><i class="fa fa-eye"></i>View</a></span></td>
 		</tr>
 	</table>
-	<p><a href=""><i class="fa fa-list"></i> See all posts</a></p>
+	<p><a href=""><i class="fa fa-list"></i>See all posts</a></p>
 </article>
 <article class="admin">
 	<h3 class="admintitle">Site Settings <span><a href=""><i class="fa fa-pencil"></i>Edit</a></span></h3>
