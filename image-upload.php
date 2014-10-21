6f8a2d396c4416edef453d575c73e8b45b39ad40
franhaselden <fran@haselden.co.uk>
2014-10-21 17:29:52 +0100

    Image uploader styling

--- image-upload.php
+++ image-upload.php
@@ -1,3 +1,10 @@
 <article id="postlist">
 	<h3 class="admintitle">Image Uploader</h3>
+	<form class="uploader aligner">
+		<div class="instructions">
+			<h3>Drag to upload</h3>
+			<i class="background fa fa-cloud-upload"></i><br />
+			<div class="chooseFilesBtn"><i class="fa fa-upload"></i>Choose files to upload</div>
+		</div>
+	</form>
 </article>
